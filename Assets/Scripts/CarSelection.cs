1ecc827f1b28aba2f99b20f3da6f288b88c420be
MrResistance <Paul1208Gush@Gmail.com>
2023-07-18 17:26:21 +0100

    Added comments to explain methods in more detail

--- Assets/Scripts/CarSelection.cs
+++ Assets/Scripts/CarSelection.cs
@@ -39,36 +39,51 @@ public class CarSelection : MonoBehaviour
         DisplayCarModel(carsListIndex);
         //Populate the car selection dropdown with the list of cars
         CreateOptionData();
+        Debug.Log(cars.Count);
     }
     private void CreateOptionData()
     {
+        //Clear the string list first to ensure no duplication of data
         carNames.Clear();
+        //We cannot directly add the car names to the dropdown so we add the list of cars to a list of strings first
         foreach (Car car in cars)
         {
             carNames.Add(car.modelPrefab.name);
         }
+        //Clear the dropdown list to ensure no duplication of data
         selectCarDropdown.ClearOptions();
+        //Add the string list we have created to the dropdown
         selectCarDropdown.AddOptions(carNames);
     }
     public void ChooseNewCar()
     {
+        //Hide our current car
         HideCarModel(carsListIndex);
+        //Set the index to the value that has been selected in the dropdown
         carsListIndex = selectCarDropdown.value;
+        //Set our currently selected car to the index value we just received
         currentlySelectedCar = cars[carsListIndex];
+        //Display the specifications and model of the car we selected
         DisplayCarInfo(currentlySelectedCar);
         DisplayCarModel(carsListIndex);
     }
     public void NextCar(bool value)
     {
+        //Set the colour of the current car back to default before we change car
         ChangeCarModelColour("#FFFFFF");
+        //Hide the current car before we change to a new one
         HideCarModel(carsListIndex);
         int nextIndex;
+        //The bool parameter we receive for this function determines whether we should be going up or down the list, true for up & false for down
         if (value == true)
         {
+            //If it's true, we set nextIndex to the modulo of the current index + 1 divded by the total count of cars
+            //E.G. If our current index is 1 and we have 3 cars total, the function will look like this (carsListIndex(1) + 1) % 3 = 2 because 2 / 3 = 0 with a remainder of 2
             nextIndex = (carsListIndex + 1) % cars.Count;
         }
         else
         {
+            //Same as above but in the other direction
             nextIndex = (carsListIndex - 1) % cars.Count;
         }
         // To ensure that the nextIndex field doesn't go outside the bounds of the array
@@ -84,6 +99,7 @@ public class CarSelection : MonoBehaviour
         currentlySelectedCar = cars[carsListIndex];
         DisplayCarInfo(currentlySelectedCar);
         DisplayCarModel(carsListIndex);
+        //To ensure the dropdown accurately reflects what car we are currently selected on
         selectCarDropdown.SetValueWithoutNotify(carsListIndex);
     }
     private void HideCarModel(int index)
@@ -119,6 +135,7 @@ public class CarSelection : MonoBehaviour
     }
     private void UpdatePrice()
     {
+        //If the car has a custom colour, the price will be 1.2x the base price
         float currentPrice = cars[carsListIndex].basePrice;
         if (customColor)
         {
